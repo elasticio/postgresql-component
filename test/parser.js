b20c4242897a5bd304c9db578c2f3609405e9c9d
Renat Zubairov <renat.zubairov@gmail.com>
2016-03-30 10:32:23 +0200

    parsing parameters and fields

--- test/parser.js
+++ test/parser.js
@@ -1,48 +1,13 @@
 var assert = require('assert');
-var sqlParser = require('sql-parser');
-var parser = sqlParser.parser;
-var lexer = sqlParser.lexer;
-var ParameterValue = sqlParser.nodes.ParameterValue;
-var Op = sqlParser.nodes.Op;
-
-/**
- * Recursive function to collect operators from Op node
- *
- * @param op operator
- * @param result resulting array
- */
-function collectParameters(op, result) {
-    if (!result) {
-        result = [];
-    }
-    if (!op || !(op instanceof Op)) return;
-    if (op.left && op.left instanceof ParameterValue) {
-        result.push(op.left);
-    } else if (op.left instanceof Op) {
-        collectParameters(op.left, result)
-    }
-    if (op.right && op.right instanceof ParameterValue) {
-        result.push(op.right);
-    } else if (op.right instanceof Op) {
-        collectParameters(op.right, result)
-    }
-    return result;
-}
+var utils = require('../lib/utils.js');
 
 describe('SQL Parser', function () {
     it('should parse statement', function () {
-        var sql = "select foo,bar from users where ((id like $test) or (foo = $baz))";
-        var tokens = lexer.tokenize(sql);
-        var stmt = parser.parse(tokens);
-        var params = collectParameters(stmt.where.conditions);
-        assert.equal(params.length, 2);
-        assert.equal(params.map((r) => r.value).join(','), 'test,baz');
-        console.log(params);
-        console.log(stmt.toString());
-
-        // parser.parse(tokens).toString()
-        //
-        // var tokens = parser.lexer.tokenize(sql);
-        // console.log(parser.parse(tokens));
+        var sql = "select foo:number, bar as boo:boolean,hasi from users where ((id like $test) or (foo = $baz:number))";
+        var result = utils.parseSQL(sql);
+        assert.equal(result.params.length, 2);
+        assert.equal(result.params.map((r) => r.value + ':' + r.type + ':' + r.index).join(','), 'baz:number:0,test:string:1');
+        assert.equal(result.fields.length, 3);
+        assert.equal(result.fields.map((r) => r.value + ':' + r.type).join(','), 'foo:number,boo:boolean,hasi:string');
     });
 });
