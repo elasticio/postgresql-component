5dec45fb3e1e864daa2df672c85f1b3cf9f03350
Renat Zubairov <renat.zubairov@gmail.com>
2016-04-02 17:20:00 +0200

    Added new property to parsing result

--- test/parser.js
+++ test/parser.js
@@ -6,7 +6,7 @@ describe('SQL Parser', function () {
         var sql = "select foo:number, bar as boo:boolean,hasi from users where ((id like $test) or (foo = $baz:number))";
         var result = utils.parseSQL(sql);
         assert.equal(result.params.length, 2);
-        assert.equal(result.params.map((r) => r.value + ':' + r.type + ':' + r.index).join(','), 'baz:number:0,test:string:1');
+        assert.equal(result.params.map((r) => r.value + ':' + r.type ).join(','), 'baz:number,test:string');
         assert.equal(result.fields.length, 3);
         assert.equal(result.fields.map((r) => r.value + ':' + r.type).join(','), 'foo:number,boo:boolean,hasi:string');
     });
@@ -17,6 +17,15 @@ describe('SQL Parser', function () {
         assert.equal(result.params.length, 0);
         assert.equal(result.fields.length, 4);
         assert.equal(result.fields.map((r) => r.value + ':' + r.type).join(','), 'id:string,name:string,url:string,phone:string');
+    });
+
+    it('should produce a runnable query', function() {
+        var sql = "select id,name,url,phone from companies where name = $name";
+        var result = utils.parseSQL(sql);
+        assert.equal(result.query, 'SELECT `id`, `name`, `url`, `phone`\n  FROM `companies`\n  WHERE (`name` = $1)');
+        assert.equal(result.params.length, 1);
+        assert.equal(result.params[0].type, 'string');
+        assert.equal(result.params[0].value, 'name');
     });
 
 });
