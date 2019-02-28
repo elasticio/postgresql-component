2b9966713ba414bcdf19845db5e0ba1ea928e5f8
Olha Virolainen <olga.shukel@gmail.com>
2019-02-28 15:25:31 +0200

    Refactor code; Add tests for utils and metadata

--- test/parser.js
+++ test/parser.js
@@ -1,75 +1,153 @@
-var assert = require('assert');
-var utils = require('../lib/utils.js');
+const assert = require('assert');
+const { expect } = require('chai');
+const utils = require('../lib/utils.js');
 
-describe('SQL Parser', function () {
-    it('should parse complex statement', function () {
-        var sql = "select foo:number, bar as boo:boolean,hasi from users where ((id like $test) or (foo = $baz:number))";
-        var result = utils.parseSQL(sql);
-        assert.equal(result.params.length, 2);
-        assert.equal(result.params.map((r) => r.value + ':' + r.type ).join(','), 'baz:number,test:string');
-        assert.equal(result.fields.length, 3);
-        assert.equal(result.fields.map((r) => r.value + ':' + r.type).join(','), 'foo:number,boo:boolean,hasi:string');
-    });
+describe('SQL Parser', () => {
+  it('should parse complex statement', () => {
+    const sql = 'select foo:number, bar as boo:boolean,hasi from users where ((id like $test) or (foo = $baz:number))';
+    const result = utils.parseSQL(sql);
+    assert.strictEqual(result.params.length, 2);
+    assert.strictEqual(result.params.map(r => `${r.value}:${r.type}`).join(','), 'baz:number,test:string');
+    assert.strictEqual(result.fields.length, 3);
+    assert.strictEqual(result.fields.map(r => `${r.value}:${r.type}`).join(','), 'foo:number,boo:boolean,hasi:string');
+  });
 
-    it('should parse simple statement without where clause', function () {
-        var sql = "select id,name,url,phone from companies";
-        var result = utils.parseSQL(sql);
-        assert.equal(result.params.length, 0);
-        assert.equal(result.fields.length, 4);
-        assert.equal(result.fields.map((r) => r.value + ':' + r.type).join(','), 'id:string,name:string,url:string,phone:string');
-    });
+  it('should parse simple statement without where clause', () => {
+    const sql = 'select id,name,url,phone from companies';
+    const result = utils.parseSQL(sql);
+    assert.strictEqual(result.params.length, 0);
+    assert.strictEqual(result.fields.length, 4);
+    assert.strictEqual(result.fields.map(r => `${r.value}:${r.type}`).join(','), 'id:string,name:string,url:string,phone:string');
+  });
 
-    it('should parse query with *', function() {
-        var sql = "select * from films";
-        var result = utils.parseSQL(sql);
-        assert.equal(result.params.length, 0);
-        assert.equal(result.fields.length, 0);
-    });
+  it('should parse query with *', () => {
+    const sql = 'select * from films';
+    const result = utils.parseSQL(sql);
+    assert.strictEqual(result.params.length, 0);
+    assert.strictEqual(result.fields.length, 0);
+  });
 
-    it('should produce a runnable query', function() {
-        var sql = "select id,name,url,phone from companies where name = $name";
-        var result = utils.parseSQL(sql);
-        assert.equal(result.query, 'SELECT id, name, url, phone\n  FROM companies\n  WHERE (name = $1)');
-        assert.equal(result.params.length, 1);
-        assert.equal(result.params[0].type, 'string');
-        assert.equal(result.params[0].value, 'name');
-    });
+  it('should produce a runnable query', () => {
+    const sql = 'select id,name,url,phone from companies where name = $name';
+    const result = utils.parseSQL(sql);
+    assert.strictEqual(result.query, 'SELECT id, name, url, phone\n  FROM companies\n  WHERE (name = $1)');
+    assert.strictEqual(result.params.length, 1);
+    assert.strictEqual(result.params[0].type, 'string');
+    assert.strictEqual(result.params[0].value, 'name');
+  });
 });
 
-describe('Parameter parsing', function() {
-    it("should handle simple list", function() {
-        var params = [
-            { value: 'foo', type: 'string' },
-            { value: 'baz', type: 'string' }
-        ];
-        var result = utils.createParametersArray(params, {
-            foo: 'bar'
-        });
-        assert.equal(result.length, 2);
-        assert.equal(typeof result[0],'string');
-        assert.equal(result[0], 'bar');
-        assert.equal(typeof result[2],'undefined');
+describe('Parameter parsing', () => {
+  it('should handle simple list', () => {
+    const params = [
+      { value: 'foo', type: 'string' },
+      { value: 'baz', type: 'string' },
+    ];
+    const result = utils.createParametersArray(params, {
+      foo: 'bar',
     });
-    it("should handle types", function() {
-        var params = [
-            { value: 'foo', type: 'string' },
-            { value: 'baz', type: 'number' },
-            { value: 'bazStr', type: 'number' },
-            { value: 'flo', type: 'float' },
-            { value: 'bool', type: 'boolean' }
-        ];
-        var result = utils.createParametersArray(params, {
-            foo: 'bar',
-            baz: 123,
-            bazStr: '123',
-            flo: '123,4',
-            bool: 'false'
-        });
-        assert.equal(result.length, 5);
-        assert.equal(typeof result[0], 'string');
-        assert.equal(typeof result[1], 'number');
-        assert.equal(typeof result[2], 'number');
-        assert.equal(typeof result[3], 'number');
-        assert.equal(typeof result[4], 'boolean');
+    assert.strictEqual(result.length, 2);
+    assert.strictEqual(typeof result[0], 'string');
+    assert.strictEqual(result[0], 'bar');
+    assert.strictEqual(typeof result[2], 'undefined');
+  });
+  it('should handle types', () => {
+    const params = [
+      { value: 'foo', type: 'string' },
+      { value: 'baz', type: 'number' },
+      { value: 'bazStr', type: 'number' },
+      { value: 'flo', type: 'float' },
+      { value: 'bool', type: 'boolean' },
+    ];
+    const result = utils.createParametersArray(params, {
+      foo: 'bar',
+      baz: 123,
+      bazStr: '123',
+      flo: '123,4',
+      bool: 'false',
     });
+    assert.strictEqual(result.length, 5);
+    assert.strictEqual(typeof result[0], 'string');
+    assert.strictEqual(typeof result[1], 'number');
+    assert.strictEqual(typeof result[2], 'number');
+    assert.strictEqual(typeof result[3], 'number');
+    assert.strictEqual(typeof result[4], 'boolean');
+  });
+});
+
+describe('Prepare query string', () => {
+  it('Should prepare Parameterized Query', () => {
+    const column1 = 525;
+    const column2 = 'Hello525';
+    const sql = 'select * from stg.testTable1 where column1 = @column1:number and column2 = @column2:string';
+    const preparedSql = {
+      text: 'select * from stg.testTable1 where column1 = $1 and column2 = $2',
+      values: [column1, column2],
+    };
+    const body = {
+      column1,
+      column2,
+    };
+    const result = utils.prepareQuery(sql, body);
+    expect(result).to.deep.equal(preparedSql);
+  });
+
+  it('Should return the same string', () => {
+    const sql = 'select * from stg.testTable1';
+    const body = {
+      column1: 5,
+      column2: 7,
+    };
+    const result = utils.prepareQuery(sql, body);
+    expect(result).to.deep.equal(sql);
+  });
+});
+
+describe('Prepare array of query string', () => {
+  it('Should prepare array of Parameterized Query and simple query string', () => {
+    const column1 = 525;
+    const column2 = 'Hello525';
+    const sql = 'select * from stg.testTable1 where column1 = @column1:number and column2 = @column2:string; select * from stg.testTable1';
+    const preparedSql = [{
+      text: 'select * from stg.testTable1 where column1 = $1 and column2 = $2',
+      values: [column1, column2],
+    }, 'select * from stg.testTable1'];
+    const body = {
+      column1,
+      column2,
+    };
+    const result = utils.prepareQueries(sql, body);
+    expect(result).to.deep.equal(preparedSql);
+  });
+
+  it('Should prepare array of two  Parameterized Queries', () => {
+    const column1 = 525;
+    const column2 = 'Hello525';
+    const column3 = 'Test';
+    const sql = 'select * from stg.testTable1 delimiter \';\' where column1 = @column1:number and column2 = @column2:string; select * from stg.testTable1 where column3 = @column3:string';
+    const preparedSql = [{
+      text: 'select * from stg.testTable1 delimiter \';\' where column1 = $1 and column2 = $2',
+      values: [column1, column2],
+    }, {
+      text: 'select * from stg.testTable1 where column3 = $1',
+      values: [column3],
+    }];
+    const body = {
+      column1,
+      column2,
+      column3,
+    };
+    const result = utils.prepareQueries(sql, body);
+    expect(result).to.deep.equal(preparedSql);
+  });
+
+  it('Should return array whit one query string', () => {
+    const sql = 'select * from stg.testTable1';
+    const body = {
+      column1: 5,
+      column2: 7,
+    };
+    const result = utils.prepareQueries(sql, body);
+    expect(result).to.deep.equal([sql]);
+  });
 });
