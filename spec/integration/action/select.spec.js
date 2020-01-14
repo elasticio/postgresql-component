11717b780b06d8a74d9ce2449ffc0b7aae358475
ElBabbler <gladikov.denis@gmail.com>
2020-01-14 15:11:06 +0200

    Fix select action script

---
+++ spec/integration/action/select.spec.js
@@ -0,0 +1,38 @@
+/* eslint-disable global-require */
+const fs = require('fs');
+const { expect } = require('chai');
+const sinon = require('sinon');
+const logger = require('@elastic.io/component-logger')();
+
+const selectAction = require('../../../lib/actions/select');
+
+let emitter;
+
+describe('Select Action test', () => {
+  if (fs.existsSync('.env')) {
+    require('dotenv').config();
+  }
+  beforeEach(() => {
+    emitter = {
+      emit: sinon.spy(),
+      logger,
+    };
+  });
+  const msg = {
+    body: {},
+  };
+  const cfg = {
+    conString: process.env.conString,
+    query: 'SELECT * FROM pg_catalog.pg_tables',
+  };
+
+  it('make select', async () => {
+    let done = false;
+    await new Promise((resolve) => {
+      emitter.emit = resolve();
+      selectAction.process.call(emitter, msg, cfg);
+      done = true;
+    });
+    expect(done).equals(true);
+  });
+});
